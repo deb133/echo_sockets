50af4769172596283c5d7f98d23979012dd0214a
darcy3e <darcy.e.balcarce@gmail.com>
2015-01-29 13:36:39 -0800

    client and server complete almost

--- echo_client.py
+++ echo_client.py
@@ -6,16 +6,19 @@ def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print >>log_buffer, 'connecting to {0} port {1}'.format(*server_address)
     # TODO: connect your socket to the server here.
+    sock.connect(server_address)
 
     # this try/finally block exists purely to allow us to close the socket
     # when we are finished with it
     try:
         print >>log_buffer, 'sending "{0}"'.format(msg)
         # TODO: send your message to the server here.
-
+        message = 'This is the message.  It will be repeated.'
+        print >>sys.stderr, 'sending "%s"' % message
+        sock.sendall(message)
         # TODO: the server should be sending you back your message as a series
         #       of 16-byte chunks.  You will want to log them as you receive
         #       each one.  You will also need to check to make sure that
@@ -24,13 +27,19 @@ def client(msg, log_buffer=sys.stderr):
         #
         #       Make sure that you log each chunk you receive.  Use the print
         #       statement below to do it. (The tests expect this log format)
-        chunk = ''
+        amount_received = 0
+        amount_expected = len(message)
+    
+        while amount_received < amount_expected:
+            chunk = sock.recv(16)
+            amount_received += len(data)
+            print >>sys.stderr, 'received "%s"' % data
         print >>log_buffer, 'received "{0}"'.format(chunk)
     finally:
         # TODO: after you break out of the loop receiving echoed chunks from
         #       the server you will want to close your client socket.
         print >>log_buffer, 'closing socket'
-
+        sock.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
